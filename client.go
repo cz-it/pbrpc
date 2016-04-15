e9e0a9971a2b56b9eb27f4a9e61473ddefe8107e
C@Z <cz.theng@gmail.com>
2016-04-15 13:19:33 +0800

    add rpc templement

--- client.go
+++ client.go
@@ -4,4 +4,74 @@
 
 package pbrpc
 
-import ()
+import (
+	"io"
+	"net"
+	"net/rpc"
+	"sync"
+)
+
+type clientCodec struct {
+	c io.Closer
+
+	// temporary work space
+	req clientRequest
+	rsp clientResponse
+
+	mutex sync.Mutex // protects pending
+}
+
+// NewClientCodec returns a new rpc.ClientCodec using JSON-RPC on conn.
+func NewClientCodec(conn io.ReadWriteCloser) rpc.ClientCodec {
+	return &clientCodec{
+		c: conn,
+	}
+}
+
+type clientRequest struct {
+}
+
+func (c *clientCodec) WriteRequest(r *rpc.Request, param interface{}) error {
+	c.mutex.Lock()
+	c.mutex.Unlock()
+	return nil
+}
+
+type clientResponse struct {
+}
+
+func (r *clientResponse) reset() {
+}
+
+func (c *clientCodec) ReadResponseHeader(r *rpc.Response) error {
+	c.mutex.Lock()
+	c.mutex.Unlock()
+
+	return nil
+}
+
+func (c *clientCodec) ReadResponseBody(x interface{}) error {
+	if x == nil {
+		return nil
+	}
+	return nil
+}
+
+func (c *clientCodec) Close() error {
+	return c.c.Close()
+}
+
+// NewClient returns a new rpc.Client to handle requests to the
+// set of services at the other end of the connection.
+func NewClient(conn io.ReadWriteCloser) *rpc.Client {
+	return rpc.NewClientWithCodec(NewClientCodec(conn))
+}
+
+// Dial connects to a PB-RPC server at the specified network address.
+func Dial(network, address string) (*rpc.Client, error) {
+	conn, err := net.Dial(network, address)
+	if err != nil {
+		return nil, err
+	}
+	return NewClient(conn), err
+}
--- server.go
+++ server.go
@@ -4,4 +4,65 @@
 
 package pbrpc
 
-import ()
+import (
+	"errors"
+	"io"
+	"net/rpc"
+	"sync"
+)
+
+var errMissingParams = errors.New("pbrpc: request body missing params")
+
+type serverCodec struct {
+	c io.Closer
+
+	mutex sync.Mutex // protects seq, pending
+}
+
+// NewServerCodec returns a new rpc.ServerCodec using JSON-RPC on conn.
+func NewServerCodec(conn io.ReadWriteCloser) rpc.ServerCodec {
+	return &serverCodec{
+		c: conn,
+	}
+}
+
+type serverRequest struct {
+}
+
+func (r *serverRequest) reset() {
+}
+
+type serverResponse struct {
+}
+
+func (c *serverCodec) ReadRequestHeader(r *rpc.Request) error {
+	c.mutex.Lock()
+	c.mutex.Unlock()
+
+	return nil
+}
+
+func (c *serverCodec) ReadRequestBody(x interface{}) error {
+	if x == nil {
+		return nil
+	}
+	return nil
+}
+
+func (c *serverCodec) WriteResponse(r *rpc.Response, x interface{}) error {
+	c.mutex.Lock()
+	c.mutex.Unlock()
+
+	return nil
+}
+
+func (c *serverCodec) Close() error {
+	return c.c.Close()
+}
+
+// ServeConn runs the PB-RPC server on a single connection.
+// ServeConn blocks, serving the connection until the client hangs up.
+// The caller typically invokes ServeConn in a go statement.
+func ServeConn(conn io.ReadWriteCloser) {
+	rpc.ServeCodec(NewServerCodec(conn))
+}
